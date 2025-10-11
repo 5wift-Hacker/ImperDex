41a9bae9636613ef9fae6e0dfa13fb242efc1498
John Newman <john.newman920@gmail.com>
2025-10-11 15:07:13 +1000

    lesson 8: design pokedex row

--- ImperDex/ContentView.swift
+++ ImperDex/ContentView.swift
@@ -10,28 +10,52 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
     
     let fetcher = FetchService()
-
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(pokedex) { pokemon in
-                    NavigationLink {
-                        VStack {
-                            Text(pokemon.name ?? "Unknown")
-                            Text("HP: \(pokemon.hp)")
+                    NavigationLink(value: pokemon) {
+                        //design the nav link here
+                        AsyncImage(url: pokemon.sprite) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
                         }
-                    } label: {
-                        Text(pokemon.name?.capitalized ?? "Unknown")
+                        .frame(width: 120, height: 120)
+                        
+                        VStack (alignment: .leading) {
+                            Text(pokemon.name!.capitalized)
+                                .fontWeight(.bold)
+                            HStack {
+                                ForEach(pokemon.types!, id: \.self) { type in
+                                    Text(type.capitalized)
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .foregroundStyle(.black)
+                                        .padding(.horizontal, 13)
+                                        .padding(.vertical, 5)
+                                        .background(Color(type.capitalized))
+                                        .clipShape(.capsule)
+                                }
+                            }
+                        }
                     }
                 }
             }
+            .navigationDestination(for: Pokemon.self) { pokemon in
+                //design the nav destination here
+            }
+            .navigationTitle("Pokedex")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
--- ImperDex/FetchService.swift
+++ ImperDex/FetchService.swift
@@ -8,31 +8,38 @@
 import Foundation
 
 struct FetchService {
-    //handle the error with an enum
+    //step 1: handle the error with an enum
     
     enum FetchError: Error {
         case badResponse
     }
     
-    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    //step 2: create the baseURL to future proof the URL usage
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
     
-    //fetch function to fetch pokemon from decoded FetchedPokemon file
-    
+    //step 3: create the fetch function
     func fetchPokemon(_ id: Int) async throws -> FetchedPokemon {
+        //create the fetch URL based on the baseURL,plus the id AS A STRING
+        //fetching each pokemon individually
         let fetchURL = baseURL.appending(path: String(id))
         
+        //step 4: handle data and response
+        //use URLSesson.shared.data and take the data from the fetchURL
+        //utilize try await
         let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
+        //step 5: handle response returns with a guard let
+        //throw the error created in the enum if error
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
             throw FetchError.badResponse
         }
         
+        //step 6: create decoder
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         
+        //step 7: create pokemon container variable
         let pokemon = try decoder.decode(FetchedPokemon.self, from: data)
-        
-        print("Fetched Pokemon: \(pokemon.id), \(pokemon.name.capitalized)")
         
         return pokemon
     }
