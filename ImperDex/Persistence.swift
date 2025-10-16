8a967e58d38cf062745b54484fc567c2bd908680
John Newman <john.newman920@gmail.com>
2025-10-16 15:54:49 +1000

    Lesson 21: finished core data app; finished widget

--- ImperDex/Persistence.swift
+++ ImperDex/Persistence.swift
@@ -52,6 +52,8 @@ struct PersistenceController {
         container = NSPersistentContainer(name: "ImperDex")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }else {
+            container.persistentStoreDescriptions.first!.url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.swifthacker.ImperDex")!.appending(path: "ImperDex.sqlite")
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
