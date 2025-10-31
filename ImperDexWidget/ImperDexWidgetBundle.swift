9ba2d30b19ae098de0d8e57e1e3c2ce4e3fb658e
John Newman <john.newman920@gmail.com>
2025-11-01 01:03:11 +1000

    Lesson 27: update widget to use swiftdata

--- ImperDexWidget/ImperDexWidgetBundle.swift
+++ ImperDexWidget/ImperDexWidgetBundle.swift
@@ -1,16 +1,16 @@
-////
-////  ImperDexWidgetBundle.swift
-////  ImperDexWidget
-////
-////  Created by John Newman on 16/10/2025.
-////
 //
-//import WidgetKit
-//import SwiftUI
+//  ImperDexWidgetBundle.swift
+//  ImperDexWidget
 //
-//@main
-//struct ImperDexWidgetBundle: WidgetBundle {
-//    var body: some Widget {
-//        ImperDexWidget()
-//    }
-//}
+//  Created by John Newman on 16/10/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ImperDexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ImperDexWidget()
+    }
+}
